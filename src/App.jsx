056567e9bd9e1b6a339ffbef1aea65f53806a198
Pratik Magdum <137306806+pratikmagdum4@users.noreply.github.com>
2024-04-27 23:22:20 +0530

    minot

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import StudentLogin from './components/StudentLogin.jsx';
 
 function App() {
-
   return (
     <>
-      <p className='border border-black'>Welcome to portal</p>
+      <div>
+        <p className='border border-black'>Welcome to the portal</p>
+        
+            {/* Correct syntax for defining a route */}
+            <StudentLogin />
+         
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/StudentLogin.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function StudentLogin()
+{
+    return <h1>
+        hi
+    </h1>;
+}
+export default StudentLogin;
