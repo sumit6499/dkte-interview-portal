2816b5b1be295cbf55d2372dc9be96cfe53d1029
Pratik Magdum <137306806+pratikmagdum4@users.noreply.github.com>
2024-05-04 20:56:18 +0530

    fix:login/signup reload

--- src/components/ui/AuthButton.jsx
+++ src/components/ui/AuthButton.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import { useNavigate } from "react-router";
 const AuthButton = ({ onClick, imageUrl, altText, buttonText, buttonUrl, isSmallScreen }) => {
+    const navigate = useNavigate();
     return (
         <button
-            onClick={() => window.location.href = buttonUrl}
+            onClick={() => navigate(buttonUrl)}
             className={`flex flex-col items-center bg-yellow-400 dark:bg-yellow-600 rounded-lg p-${isSmallScreen ? '2' : '4'} shadow-lg animate-slideFromLeft`}
         >
             <img
